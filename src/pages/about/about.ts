18653fd6449c7791764a435996f9c31561f94dbb
Sunine <962673406@qq.com>
2017-05-01 18:51:08 +0800

    choose pics

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,23 +1,59 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { ViewChild } from '@angular/core';
-import { Content } from 'ionic-angular';
+import { Component,ViewChild} from '@angular/core';
 
 
-
+import { NavController} from 'ionic-angular';
+import { Camera } from 'ionic-native';
+import { Slides } from 'ionic-angular';//注入轮播
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-export class AboutPage {
-  @ViewChild(Content) content: Content;
+export class AboutPage{
+  @ViewChild(Slides) slides: Slides;
 
+  public path;
+  pics:Pic[]=[
+  	{path:"assets/img/default.png"}
+  ];
+  count:number=0;
   constructor(public navCtrl: NavController) {
+  	
+  }
+  choosePhoto() {
+    var options = {
+      // Some common settings are 20, 50, and 100
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      // In this app, dynamically set the picture source, Camera or photo gallery
+      sourceType:0,//0对应的值为PHOTOLIBRARY ，即打开相册
+      encodingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      allowEdit: true,
+      correctOrientation: true  //Corrects Android orientation quirks
+    }
+    if(this.count<5){
+    	Camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image =  imageData;
+      this.path = base64Image;
+      if(this.count===0){
+        this.pics.pop();
+      }
+      this.count++;
+      this.pics.push({
+      		path:base64Image
+      });
+    }, (err) => {
+      // Handle error
+    });
+    }else{
+    	alert("已到达上传数量限制");
+    }
+    
 
   }
-
-  scrollToBottom() {
-    this.content.scrollToBottom();
-  }
-
+}
+export class Pic{
+	path:string;
 }
