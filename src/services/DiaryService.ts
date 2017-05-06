b0f12122b462e5c2076c018b991273f0fc9602dc
Sunine <962673406@qq.com>
2017-05-06 15:52:07 +0800

    modify writeDiary

--- src/services/DiaryService.ts
+++ src/services/DiaryService.ts
@@ -7,14 +7,14 @@ import 'rxjs/add/operator/map';
 export class DiaryService {
   url='http://dev.note.tunnel.qydev.com/diary';
   constructor(private http: Http) { }
-  writeDiary(title,userId,content) {
+  writeDiary(title,userId,content,weather) {
     let headers = new Headers({
             'Content-Type': 'application/x-www-form-urlencoded'
         });
         let options = new RequestOptions({
             headers: headers
         });
-    let body= "method=writeDiary&title="+title+"&userId="+userId+"&content="+content;
+    let body= "method=writeDiary&title="+title+"&userId="+userId+"&content="+content+"&weather="+weather;
     return new Promise((resolve, reject) => {
       this.http.post(this.url, body, options )
         .map(res => res.json())
